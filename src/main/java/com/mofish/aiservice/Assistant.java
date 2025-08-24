326ab8990e41fcb06466c1fd7cbd1ebc92893e7e
radeonvii <radeonk@foxmail.com>
2025-08-24 19:35:23 +0800

    feat：用户提示词使用

--- src/main/java/com/mofish/aiservice/Assistant.java
+++ src/main/java/com/mofish/aiservice/Assistant.java
@@ -1,5 +1,7 @@
 package com.mofish.aiservice;
 
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -7,5 +9,9 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 @AiService(wiringMode = EXPLICIT, chatModel = "qwenChatModel", chatMemory = "chatMemory")
 public interface Assistant {
 
-    String chat(String userMessage);
+//    @UserMessage("你是我的朋友，请用文言文风格回答问题。{{it}}")
+//    String chat(String userMessage);
+
+    @UserMessage("你是我的朋友，请用动漫傲娇女主风格回答问题。{{userMessage}}")
+    String chat(@V("userMessage") String userMessage);
 }
